2b5b198b02dad128a2aa84f641be90c608fa63b5
kiran kumar PAMPANA <kiran.pampana@gmail.com>
2017-07-10 17:30:32 -0400

    modified db password

--- app/app.py
+++ app/app.py
@@ -7,7 +7,7 @@ mysql = MySQL()
 app = Flask(__name__)
 app.config['MYSQL_HOST'] = sys.argv[1]
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = '1234567'
+app.config['MYSQL_PASSWORD'] = 'password'
 app.config['MYSQL_DB'] = 'tel_dir'
 mysql.init_app(app)
 
